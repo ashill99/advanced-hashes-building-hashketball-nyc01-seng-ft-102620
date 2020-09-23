da0356c8010a2eacf9aa2de6f8131c402d2b1d8d
Adam Shilling <admshng@gmail.com>
2020-09-23 22:19:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,7 +7,7 @@ def game_hash
     :colors => {},
     :players =>  [
       {:player_name => "Alan Anderson", :assists => "12", :blocks => "1", :number => "0", :points => "22", :rebounds => "12", :shoe => "16", :slam_dunks => "1", :steals => "3"},
-    {:player_name => "Alan Anderson", :assists => "12", :blocks => "1", :number => "0", :points => "22", :rebounds => "12", :shoe => "16", :slam_dunks => "1", :steals => "3"}]
+    {:player_name => "Reggie Evans", :assists => "12", :blocks => "12", :number => "30", :points => "12", :rebounds => "12", :shoe => "14", :slam_dunks => "7", :steals => "12"}]
     },
     :away => {
     :team_name => "",
