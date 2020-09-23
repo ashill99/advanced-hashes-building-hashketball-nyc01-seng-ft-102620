7fe64fe30d8d0d3eb72457eb685d81ffba723bf2
Adam Shilling <admshng@gmail.com>
2020-09-23 22:11:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     :home => {
     :team_name => "Brooklyn Nets",
     :colors => {},
-    :players =>  [{:player_name => "Alan Anderson"}]
+    :players =>  [{:player_name => "Alan Anderson"}, {:assists: => ""}]
     },
     :away => {
     :team_name => "",
