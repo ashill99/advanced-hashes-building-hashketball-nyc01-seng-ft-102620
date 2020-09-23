80fc929fb878af058fec070738641d094d1ae68d
Adam Shilling <admshng@gmail.com>
2020-09-23 22:02:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,18 @@
+require 'pry' 
 # Write your code here!
+def game_hash
+  final_hash = {
+    :home => {
+    :team_name => "",
+    :colors => {},
+    :players => [{:player_name => "Alan Anderson"}, {
+      :assists => 
+    }
+    ]},
+    :away => {
+    :team_name => "",
+    :colors => {},
+    :players => []
+    }
+  }
+end 
